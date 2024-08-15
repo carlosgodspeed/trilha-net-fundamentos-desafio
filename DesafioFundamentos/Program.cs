82f19009f77933bde9ef93056a9600aa18372f62
carlosgodspeed <carlosminibics@gmail.com>
2024-08-15 15:07:26 -0300

    Finalizado

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,6 +6,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
+// Solicita ao usuário o preço inicial e o preço por hora
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
@@ -32,28 +33,35 @@ while (exibirMenu)
     switch (Console.ReadLine())
     {
         case "1":
+            // Chama o método para adicionar um veículo
             es.AdicionarVeiculo();
             break;
 
         case "2":
+            // Chama o método para remover um veículo
             es.RemoverVeiculo();
             break;
 
         case "3":
+            // Chama o método para listar os veículos
             es.ListarVeiculos();
             break;
 
         case "4":
+            // Encerra o loop e finaliza o programa
             exibirMenu = false;
             break;
 
         default:
+            // Caso o usuário digite uma opção inválida
             Console.WriteLine("Opção inválida");
             break;
     }
 
+    // Pausa para que o usuário possa ver a mensagem antes de limpar a tela
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
 
+// Mensagem final ao encerrar o programa
 Console.WriteLine("O programa se encerrou");
